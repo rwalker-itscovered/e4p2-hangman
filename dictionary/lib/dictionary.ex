f53583fd5d0032fa986fb8fe13f98989d30efdd0
rwalker-itscovered <rwalker@itscovered.com>
2024-06-13 17:04:16 -0400

    adding the word list, getting a random word

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,5 +1,11 @@
 defmodule Dictionary do
-  def hello do
-    IO.puts "Hello world!!"
+
+  def word_list do
+    File.read!("assets/words.txt")
+    |> String.split(~r/\n/, trim: true)
+  end
+
+  def random_word do
+    Enum.random(word_list())
   end
 end
--- dictionary/test/dictionary_test.exs
+++ dictionary/test/dictionary_test.exs
@@ -2,7 +2,13 @@ defmodule DictionaryTest do
   use ExUnit.Case
   doctest Dictionary
 
-  test "greets the world" do
-    assert Dictionary.hello() == :world
+  test "word_list returns a list of words" do
+    [ first | _] = Dictionary.word_list()
+    assert first == "that"
+  end
+
+  test "random_word returns a random word from the list" do
+    :rand.seed(:exsss, {100, 101, 102})
+    assert Dictionary.random_word() == "launches"
   end
 end
