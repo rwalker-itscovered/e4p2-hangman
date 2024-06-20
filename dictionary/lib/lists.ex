ba7898a60b9c10c20b5fe0f8f6ea96954236f8aa
rwalker-itscovered <rwalker@itscovered.com>
2024-06-20 17:13:20 -0400

    pattern matching - even length

--- dictionary/lib/lists.ex
+++ dictionary/lib/lists.ex
@@ -13,4 +13,12 @@ defmodule Lists do
 
   def map([], _func), do: []
   def map([h | t], func), do: [func.(h) | map(t, func)]
+
+  def sum_pairs([]), do: []
+  def sum_pairs([ h | t = []]), do: [h | sum_pairs(t)]
+  def sum_pairs([h1, h2 | t]), do: [h1+h2 | sum_pairs(t)]
+
+  def even_length([]), do: true
+  def even_length([ _h | []]), do: false
+  def even_length([_h1, _h2 | t]), do: even_length(t)
 end
--- dictionary/test/lists_test.exs
+++ dictionary/test/lists_test.exs
@@ -39,4 +39,21 @@ defmodule ListsTest do
       assert Lists.map( [2, 8, 16], &(&1 / 2)) == [1, 4, 8]
     end
   end
+
+  describe "sum_pairs/1" do
+    test "returns the sums of pairs" do
+      assert Lists.sum_pairs([]) == []
+      assert Lists.sum_pairs([1, 3, 5, 7]) == [4, 12]
+      assert Lists.sum_pairs([1, 3, 5, 7, 9]) == [4, 12, 9]
+    end
+  end
+
+  describe "even_length/1" do
+    test "returns true if even number of elements" do
+      assert Lists.even_length([])
+      assert Lists.even_length([1,2])
+      refute Lists.even_length([1, 3, 5])
+    end
+  end
+
 end
