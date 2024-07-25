2d7aac8204c3d0a7993dc4e9a0437b1798ecc1cd
rwalker-itscovered <rwalker@itscovered.com>
2024-07-25 08:32:54 -0400

    working agent dictionary

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,10 +1,10 @@
 defmodule Dictionary do
-  alias Dictionary.Impl.WordList
+  alias Dictionary.Impl.AgentWordList
   alias Dictionary.Type
 
   @spec start() :: Type.word_list()
-  defdelegate start, to: WordList
+  defdelegate start, to: AgentWordList
 
   @spec random_word(Type.word_list()) :: Type.word()
-  defdelegate random_word(word_list), to: WordList
+  defdelegate random_word(word_list), to: AgentWordList
 end
--- dictionary/lib/type.ex
+++ dictionary/lib/type.ex
@@ -1,4 +1,4 @@
 defmodule Dictionary.Type do
-  @type word_list :: list(word)
+  @type word_list :: list(word) | pid()
   @type word :: String.t()
 end
